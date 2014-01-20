2642447ad8b5925f85ad6e325b7631e2b906f344
Arif De Sousa <arifads@gmail.com>
2014-01-20 11:23:30 -0430

    Initial Commit

---
+++ Estructuras Jerarquicas/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Estructuras Jerarquicas
+//
+//  Created by Arif De Sousa on 1/20/14.
+//  Copyright (c) 2014 ADS Studios. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
